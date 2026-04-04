f32e6d2cb48f79038efefe39b90624fafb5b18c8
ShreyaYaganti <60340160+ShreyaYaganti@users.noreply.github.com>
2026-04-04 17:09:22 +0530

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    boolean dp[];
+    public boolean canPartition(int[] nums) {
+        int sum =0;
+      
+        for(int num:nums){
+            sum = sum+num;
+        }
+        if(sum%2!=0)return false;
+        sum = sum/2;
+         dp = new boolean[sum+1];
+           Arrays.fill(dp,false);
+        return canSum(nums,0,sum);
+        
+    }
+
+    private boolean canSum(int[]nums, int i,int sum){
+        if(i<0||i>=nums.length||sum<0)return false;
+        if(sum==0)return true;
+        if(dp[sum])return true;
+       return   canSum(nums,i+1,sum-nums[i]) ||canSum(nums,i+1,sum);
+    }
+}
